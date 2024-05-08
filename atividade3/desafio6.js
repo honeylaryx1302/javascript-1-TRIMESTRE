852693f2b70e0c2033653b8527dd539628c723fd
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-05-08 11:59:44 -0300

    Add files via upload

---
+++ atividade3/desafio6.js
@@ -0,0 +1,8 @@
+const salaJS = ["Evaldo", "Camis","Mari"];
+const salaPython =  ["ju", "Leo", "Raquel"];
+
+const SalasUnificadas = salaJS.concat(salaPython);
+const SalasUnificadas2 = salaPython.concat(salaJS);
+
+console.log(SalasUnificadas)
+console.log(SalasUnificadas2)
---
+++ atividade3/desafio7.js
@@ -0,0 +1,14 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listadeAlunosEMedias =  [alunos,medias]
+
+console.log(
+     `A aluna da posição 1 da lista de alunos é:${listadeAlunosEMedias[0][1]}
+      A nota dessa aluna é ${listadeAlunosEMedias[1][1]}`
+);
+
+console.log(
+    `A aluna da posição 2 da lista de alunos é:${listadeAlunosEMedias[0][2]}
+     A nota dessa aluna é ${listadeAlunosEMedias[1][2]}`
+);
